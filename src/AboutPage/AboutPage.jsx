fdc07a8de288eac92ca508c9312d507a04eb1fdf
Chris L <c.aventine@gmail.com>
2021-10-27 11:57:16 -0500

    Pic

--- src/AboutPage/AboutPage.jsx
+++ src/AboutPage/AboutPage.jsx
@@ -8,7 +8,7 @@ class AboutPage extends Component {
                 <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <pattern id="img2" patternUnits="userSpaceOnUse" width="100" height="100">
-                            <image href="forest copy.jpg" x="-25" width="150" height="100" />
+                            <image href="CL_HS.jpg" x="-25" width="150" height="100" />
                         </pattern>
                     </defs>
                     <polygon id="hex" points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img2)" />
